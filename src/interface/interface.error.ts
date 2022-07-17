88b713febfaa502711cd30aaae497e0cdf2ed966
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-17 01:19:32 -0300

    feat: Criação de Interface de Usuário e Erros

---
+++ src/interface/interface.error.ts
@@ -0,0 +1,6 @@
+export interface IError {
+  error: {
+    status: number;
+    message: string;
+  }
+}
---
+++ src/interface/interface.user.ts
@@ -0,0 +1,11 @@
+export interface IBalance {
+  name:string,
+  id:number,
+  wallet:{balance:number}
+}
+
+export interface IUserBalance {
+  id:number,
+  name:string,
+  balance:number
+}
