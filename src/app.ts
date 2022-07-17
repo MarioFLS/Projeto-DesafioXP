b386329d1699ee2b3fc22171fe68829894162560
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-17 01:18:52 -0300

    chore: Configuração do express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from './express';
 import 'dotenv/config';
+import clientRoute from './routes/route.client';
 
 const port = process.env.APP_PORT || 3002;
+
+express.use('/user', clientRoute);
 
 express.listen(port, () => console.log('Olá, você está na porta', port));
--- src/express.ts
+++ src/express.ts
@@ -2,6 +2,7 @@ import expressLib from 'express';
 import 'express-async-errors';
 
 const express = expressLib();
+
 express.use(expressLib.json());
 
 export default express;
