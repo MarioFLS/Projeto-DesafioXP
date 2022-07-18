c581cbb6f301a290d2dc23c99f332f6f8352f6b4
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-18 13:48:30 -0300

    perf: Implementação da validação quando for ver o Seu Saldo

--- src/routes/route.client.ts
+++ src/routes/route.client.ts
@@ -7,7 +7,7 @@ import TokenValidate from '../middleware/validate.token';
 const clienteRoute = express.Router();
 
 clienteRoute.post('/conta/login', UserLogin.login);
-clienteRoute.get('/conta/:id', UserBalance.getBalance);
+clienteRoute.get('/conta/:id', TokenValidate, UserBalance.getBalance);
 clienteRoute.put('/conta/deposit/:id', TokenValidate, UserDeposit.setDeposit);
 
 export default clienteRoute;
