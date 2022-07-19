d98c2ee5f014f131ba7f37cd63d6dfa655f0e89c
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-19 16:28:39 -0300

    feat: Criação de rotas pra saque

--- src/routes/route.client.ts
+++ src/routes/route.client.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import UserBalance from '../controllers/UserController/controllers.get.balance';
 import UserLogin from '../controllers/UserController/controllers.login.user';
-import UserDeposit from '../controllers/UserController/controllers.set.deposit';
+import UserDeposit from '../controllers/UserController/controllers.set.balance';
 import TokenValidate from '../middleware/validate.token';
 
 const clienteRoute = express.Router();
 
 clienteRoute.post('/conta/login', UserLogin.login);
 clienteRoute.get('/conta', TokenValidate, UserBalance.getBalance);
-clienteRoute.post('/conta/deposit', TokenValidate, UserDeposit.setDeposit);
+clienteRoute.post('/conta/deposito', TokenValidate, UserDeposit.setBalance);
+clienteRoute.post('/conta/saque', TokenValidate, UserDeposit.setBalance);
 
 export default clienteRoute;
