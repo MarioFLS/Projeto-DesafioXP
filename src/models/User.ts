6435b4de63e95f9346acb1de740019aaed56e0a4
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-19 23:55:15 -0300

    refactor: Model user. Mundo a associação dos assets

--- src/models/User.ts
+++ src/models/User.ts
@@ -50,12 +50,12 @@ User.init(
 Wallet.belongsTo(User, { as: 'user', foreignKey: 'user_id' });
 User.hasOne(Wallet, { as: 'wallet', foreignKey: 'user_id' });
 
-User.hasMany(UserAssets, { as: 'userAssets', foreignKey: 'user_id' });
+User.hasMany(UserAssets, { as: 'Assets', foreignKey: 'user_id' });
 UserAssets.belongsToMany(User, {
   as: 'user', foreignKey: 'user_Id', through: Asset, otherKey: 'asset_id',
 });
 
-User.hasMany(UserHistory, { as: 'userHistory', foreignKey: 'user_id' });
+User.hasMany(UserHistory, { as: 'History', foreignKey: 'user_id' });
 UserHistory.belongsToMany(User, {
   as: 'user', foreignKey: 'user_Id', through: UserHistory, otherKey: 'asset_id',
 });
