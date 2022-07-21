46e8bfd3eeb543cb0ba90f829738da0c5b9852c1
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-21 16:55:38 -0300

    teste: Teste de Service: testando a carteira do cliente

---
+++ test/service/User/get.balance.test.ts
@@ -0,0 +1,20 @@
+import chai from 'chai';
+import GetBalance from '../../../src/service/User/get.balance';
+
+const { expect } = chai;
+
+describe("Teste Da carteira do Usuário Usuário >>>>>>>>>>>>>>>>>>>>>>>>>>>>> ", () => {
+    it("Testando se a carteira do Cliente vem Correta", async () => {
+        const response = await GetBalance.getBalance(1, 'Pedro Jorge');
+        expect(response).to.deep.equal({ id: 1, name: 'Pedro Jorge', balance: '250.00' });
+    });
+    it("Caso não exista um usuário e ela retorne um erro", async () => {
+      let getError: Error | GetBalance;
+      try {
+        getError = await GetBalance.getBalance(166, 'fake');
+      } catch (error) {
+      getError = error;
+      }
+      expect(getError).to.be.an('error');
+  })
+})
