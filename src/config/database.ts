9cbf02e80a5042c8fef5aa553586ca26f1d36403
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-21 14:31:32 -0300

    chore: Implementação de bibiloteca para test

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,7 @@ import { Options } from 'sequelize';
 const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD,
-  database: 'investimentosApi',
+  database: process.env.DB_TEST || 'investimentosApi',
   host: process.env.DB_HOST || 'localhost',
   port: 3306,
   dialect: 'mysql',
