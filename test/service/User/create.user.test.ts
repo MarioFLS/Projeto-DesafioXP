f41fce94cf7fc3137b478cb97090d2be76d8daf8
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-21 23:34:15 -0300

    test: Criação de teste para cirar usuário

---
+++ test/service/User/create.user.test.ts
@@ -0,0 +1,47 @@
+import chai from 'chai';
+import shell from 'shelljs';
+import jwt from 'jsonwebtoken';
+import { restoreDatabase } from '../../helpers/comand';
+import { userfake } from '../../helpers/mock.format.user';
+import CreateUser from '../../../src/service/User/create.user';
+import { INewUser } from '../../../src/interface/interface.user';
+import User from '../../../src/models/User';
+import { StatusCodes } from 'http-status-codes';
+
+const { expect } = chai;
+
+describe('Testando se é possivel criar um Usuário >>> ', () => {
+  beforeAll(async () => {
+    await shell.exec(restoreDatabase);
+  });
+
+  afterAll(async () => {
+    await shell.exec(restoreDatabase);
+  });
+
+  it('Criando um falso usuário', async () => {
+    const newFakeUser = { ...userfake, saldo: 100 } as INewUser;
+    const response = (await CreateUser(newFakeUser)) as string;
+    const verifyJwt = jwt.verify(
+      response,
+      process.env.SECRET_PASSWORD as string
+    );
+    expect(verifyJwt).to.deep.contains.keys('id', 'name', 'email');
+    const { id } = verifyJwt as { id: number };
+
+    const user = await User.findOne({ where: { id } });
+    expect(user?.toJSON()).to.deep.contains({ email: newFakeUser.email, id });
+  });
+
+  it('Esperando erro ao criar um usuário que já existe', async () => {
+    const newFakeUser = { ...userfake, saldo: 100 } as INewUser;
+    const response = (await CreateUser(newFakeUser)) as string;
+
+    expect(response).to.deep.equal({
+      error: {
+        code: StatusCodes.UNAUTHORIZED,
+        message: 'Esse email de usuário já possui conta! Tente outro!',
+      },
+    });
+  });
+});
--- test/service/User/get.balance.test.ts
+++ test/service/User/get.balance.test.ts
@@ -3,7 +3,7 @@ import GetBalance from '../../../src/service/User/get.balance';
 
 const { expect } = chai;
 
-describe("Teste Da carteira do Usuário Usuário >>>  ", () => {
+describe("Teste Da carteira do Usuário >>>  ", () => {
     it("Testando se a carteira do Cliente vem Correta", async () => {
         const response = await GetBalance.getBalance(1, 'Pedro Jorge');
         expect(response).to.deep.equal({ id: 1, name: 'Pedro Jorge', balance: '250.00' });
