4b62d223527680a117e4698c66339a764bbb446e
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-21 21:22:12 -0300

    test: Teste de deletar e atualizar o cliente

---
+++ test/helpers/mock.format.user.ts
@@ -0,0 +1,11 @@
+import { INewUser } from "../../src/interface/interface.user";
+
+const nameFake = 'Nome muito fake';
+const emailFake = 'emailfake@gmail.com';
+const senhaFake = 'senhamuitoforte';
+
+export const userfake = {
+  name: nameFake,
+  email: emailFake,
+  password: senhaFake,
+} as INewUser;
--- test/service/User/update.user.test.ts
+++ test/service/User/update.user.test.ts
@@ -1,23 +1,15 @@
 import chai from 'chai';
 import jwt from 'jsonwebtoken';
 const shell = require('shelljs');
-import { INewUser } from '../../../src/interface/interface.user';
 import UserUpdate from '../../../src/service/User/update.user';
 import { restoreDatabase } from '../../helpers/comand';
 import User from '../../../src/models/User';
+import { userfake } from '../../helpers/mock.format.user';
 
 const { expect } = chai;
 
 describe('Testando atualização do Usuário Usuário >>> ', () => {
-  const nameFake = 'Nome muito fake';
-  const emailFake = 'emailfake@gmail.com';
-  const senhaFake = 'senhamuitoforte';
-  
-  const userfake = {
-    name: nameFake,
-    email: emailFake,
-    password: senhaFake,
-  } as INewUser;
+  const fake = userfake;
 
   beforeEach(async () => {
     await shell.exec(restoreDatabase);
@@ -29,7 +21,7 @@ describe('Testando atualização do Usuário Usuário >>> ', () => {
 
   it('Testando se é possível atualizar o usuário', async () => {
     const response = await UserUpdate(userfake, 2);
-    const user = await User.findOne({ where: { email: emailFake, id:2 } });
+    const user = await User.findOne({ where: { email: fake.email, id:2 } });
     const result = user?.toJSON();
 
     
