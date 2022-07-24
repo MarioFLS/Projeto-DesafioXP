a88c6984afaa1aff985906590ed5b0f99c798108
MarioFLS <mariofernandolsantos@gmail.com>
2022-07-23 21:52:56 -0300

    fix: swagger posto do lado errado

--- src/routes/route.investiment.ts
+++ src/routes/route.investiment.ts
@@ -6,12 +6,79 @@ import ValidateInvestiment from '../middleware/middleware.investiment';
 
 const investimentRoute = express.Router();
 
+/**
+ * @swagger
+ *  tags:
+ *      name: Investimento
+ *      description: Endpoints de investimentos
+ */
+
 investimentRoute.post('/comprar', TokenValidate, ValidateInvestiment, InvestimentBuy.buy);
+/**
+ * @swagger
+ *  /investimentos/comprar:
+ *    post:
+ *      tags: [Investimento]
+ *      description: Retorna o sucesso da compra do ativo
+ *      requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: integer
+ *                    example: 1
+ *                  quantity:
+ *                    type: integer
+ *                    example: 2
+ *      responses:
+ *        200:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: Ativo adquirido com sucesso
+ */
 investimentRoute.post(
   '/vender',
   TokenValidate,
   ValidateInvestiment,
   InvestimentSale.sale,
 );
+/**
+ * @swagger
+ *  /investimentos/vender:
+ *    post:
+ *      tags: [Investimento]
+ *      description: Retorna o sucesso da compra do ativo
+ *      requestBody:
+ *          required: true
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: integer
+ *                    example: 1
+ *                  quantity:
+ *                    type: integer
+ *                    example: 2
+ *      responses:
+ *        200:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    example: Ativo vendido com sucesso
+ */
 
 export default investimentRoute;
